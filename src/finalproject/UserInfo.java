9badba53f01f7d5dcbee2fe2fbe6b016102343cb
lborg019 <lborg019@jccl-53.ad.cs.fiu.edu>
2014-11-19 19:32:01 -0500

    UserInfo scans for the user's reservation info. and stores it to local variables.

--- src/finalproject/UserInfo.java
+++ src/finalproject/UserInfo.java
@@ -18,5 +18,53 @@ import java.io.*;
 
 public class UserInfo
 {
+    int uNumber;
+    String uName;
+    String uDate;
+    String uTime;
+    
+    public UserInfo()
+    {
+        
+    }
+
+    public UserInfo(int uNumber, String uName, String uDate, String uTime)
+    {        
+        this.uNumber = uNumber;
+        this.uName = uName;
+        this.uDate = uDate;
+        this.uTime = uTime;
+    }
+    
+    public void loadInfo()
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please type in your name");
+        setuName(scanner.nextLine());
+        System.out.println("Please type in the reservation date");
+        setuDate(scanner.nextLine());
+        System.out.println("Please type in the reservation"
+                           + " time (continuous format)");
+        setuTime(scanner.nextLine());
+    }
+    
+    //Getters
+    public int getuNumber() { return uNumber; }
+    public String getuName(){ return uName;   }
+    public String getuDate(){ return uDate;   }
+    public String getuTime(){ return uTime;   }
+    
+    //Setters
+    public void setuNumber(int rNumber){ this.uNumber = rNumber; }    
+    public void setuName(String rName){ this.uName    = rName;   }
+    public void setuDate(String rDate){ this.uDate    = rDate;   }
+    public void setuTime(String rTime){ this.uTime    = rTime;   }    
+    
+    
+    
+    
+    
+    
+    
     
 }
