a746bff5768a193f84987c57c6c2763974706bd5
Lukezin <lukasborges@gmail.com>
2014-12-02 01:04:49 -0500

    ListIterator interface

---
+++ src/finalproject/ListIterator.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package finalproject;
+
+/**
+ *
+ * @author Owner
+ */
+public interface ListIterator
+{
+    //move iterator past the next element
+    //return traversed element
+    Object next();
+    
+    //tests if there is an element after position
+    //return true if such element exists
+    boolean hasNext();
+    
+    //adds ane element before the iterator position
+    //moves the iterator past inserted element
+    //parameter = element to add
+    void add(ReservationInfo element);
+    
+    //Removes the last traversed element.
+    //This method can only be called after call to next method
+    void remove();
+    
+    //Sets the last traversed element to a different value
+    //parameter = element to set
+    void set(ReservationInfo element);
+}
