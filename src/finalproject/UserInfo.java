ef1c0a8c65db896807a70bc686c16e10eaa1dadc
Lukezin <lukasborges@gmail.com>
2014-12-03 01:53:52 -0500

    Class adapted for time only.

--- src/finalproject/UserInfo.java
+++ src/finalproject/UserInfo.java
@@ -18,49 +18,20 @@ import java.io.*;
 
 public class UserInfo
 {
-    //int uNumber;
     String uName;
-    String uDate;
     String uTime;
     
-    public UserInfo()
-    {
-        
-    }
+//    public UserInfo()
+//    {
+//        
+//    }
 
-    public UserInfo(String uName, String uDate, String uTime)
+    public UserInfo(String uName, String uTime)
     {        
-        //this.uNumber = uNumber;
         this.uName = uName;
-        this.uDate = uDate;
         this.uTime = uTime;
     }
-    
-    //check if date is okay
-    public boolean checkDate(String date)
-    {
-        //date follows mm/dd/yyyy format;
-        //fail validation test if:
-        //-> less than 10 characters are entered;
-        //-> slashes are placed incorrectly
-        //-> day > 31
-        //-> month > 12
-        // no checks for months with 30 or 31 days
-        
-        int day = Integer.parseInt(date.substring(0,2));
-        int month = Integer.parseInt(date.substring(3,5));
-        
-        if(date.charAt(2) != '/' && date.charAt(4) != '/'
-           || date.length() != 10 || day > 12 || month > 31)
-        {
-            return false; //fails
-        }
-        else
-        {
-            return true; //succeeds
-        }
-    }
-    
+
     //check if time is okay
     public boolean checkTime(String time)
     {
@@ -93,17 +64,6 @@ public class UserInfo
         System.out.println("Please type in your name:");
         setuName(scanner.nextLine());
         
-        System.out.println("Please type in the reservation date [mm/dd/yyyy]:");
-        tempDate = scanner.nextLine();
-        while(checkDate(tempDate) == false) //input validation loop
-        {
-            System.out.println("Invalid date.");
-            System.out.println("Please type the reservation date [mm/dd/yyyy]");
-            tempDate = scanner.nextLine();
-        }
-        setuDate(tempDate); //sets validated date
-        
-        
         System.out.println("Please type in the reservation"
                              + " time [hh:mm continuous format]:");
         tempTime = scanner.nextLine();
@@ -117,16 +77,11 @@ public class UserInfo
     }
     
     //Getters
-    //public int getuNumber() { return uNumber; }
     public String getuName(){ return uName;   }
-    public String getuDate(){ return uDate;   }
     public String getuTime(){ return uTime;   }
     
     //Setters
-    //public void setuNumber(int rNumber){ this.uNumber = rNumber; }    
-    
     public void setuName(String rName){ this.uName    = rName;   }
-    public void setuDate(String rDate){ this.uDate    = rDate;   }
     public void setuTime(String rTime){ this.uTime    = rTime;   }
     
 }
